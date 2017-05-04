112b34563741215517734495ff6247bbe3ebc6b0
abdul <mohammedabdulbari92@gmail.com>
2017-05-04 13:51:32 -0500

    Registration page UI

--- app/src/main/java/com/example/jaden/medicoapp/RegistrationActivity.java
+++ app/src/main/java/com/example/jaden/medicoapp/RegistrationActivity.java
@@ -4,9 +4,11 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -310,6 +312,17 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
             VolleyController.getInstance().addToRequestQueue(jsonObjectRequest);
         }else{
             Toast.makeText(this, "There is some error try again",Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
